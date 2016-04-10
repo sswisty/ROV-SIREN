0502de3345841a3469dfbdde69cb6851dc884b63
Shawn Swist <sswisty7@gmail.com>
2016-04-09 20:48:31 -0400

    Python Xbox Controller

--- Python-Codes/XboxControllerReadValues.py
+++ Python-Codes/XboxControllerReadValues.py
@@ -6,6 +6,8 @@ In actual code start controller with
     XboxCont = XboxController( deadzone = ##, scale = ##, invertYAxis = True)
 where ## represent the max value of the triggers/joysticks and the size of the deadzone
 
+Use GetValues() to read all values from the controller
+
 Provides all values from the xbox controller into code except left thumb...
 
 Pygame must be downloaded for this code to work and can be downloaded at:
